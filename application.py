682331ac262395ac835030db06ecfbf859945aa7
松田和樹 <matsuda.kazuki.55m@st.kyoto-u.ac.jp>
2022-04-12 13:09:55 +0900

    型ヒントや変数名変更など

--- application.py
+++ application.py
@@ -16,115 +16,134 @@ app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 
 db = SQL("sqlite:///project.db")
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         is_error = False
+
+        request_housework_name = request.form.get("housework_name")
+        request_interval = request.form.get("interval")
+        request_day_week_month = request.form.get("day_week_month")
+
         # Ensure housework_name was submitted
-        if not request.form.get("housework_name"):
+        if not request_housework_name:
             flash("Please input housework name!")
             is_error = True
 
         # Ensure interval was submitted
-        if not request.form.get("interval"):
+        if not request_interval:
             flash("Please input interval!")
             is_error = True
 
         # Store housework names in a list
-        housework_names = db.execute("SELECT housework_name FROM housework")
-        names = []
-        for i in range(len(housework_names)):
-            names.append(housework_names[i]["housework_name"])
+        housework_names_queryset = db.execute(
+            "SELECT housework_name FROM housework")
+        housework_names = [housework_names_queryset[i]['housework_name']
+                           for i in range(len(housework_names_queryset))]
 
         # Ensure the housework already exists
-        if request.form.get("housework_name") in names:
+        if request_housework_name in housework_names:
             flash("The housework already exists!")
             is_error = True
 
         # Accept only positive numbers
-        if not request.form.get("interval").isnumeric() or request.form.get("interval") == 0:
+        if not request_interval.isnumeric() or request_interval == 0:
             flash("Please input natural number!")
             is_error = True
 
         # Add the new housework
         if not is_error:
             db.execute("INSERT INTO housework (housework_name, interval, day_week_month) VALUES (?, ?, ?)",
-                       request.form.get("housework_name"), request.form.get("interval"), request.form.get("day_week_month"))
+                       request_housework_name, request_interval, request_day_week_month)
 
     # Get housework data
-    housework_data = db.execute("SELECT * FROM housework")
+    housework_queryset = db.execute("SELECT * FROM housework")
 
     # Get Today's date
     today = datetime.date.today()
 
-    for housework_datum in housework_data:
+    for housework in housework_queryset:
         # Converts the last notification date to a date type
-        time = datetime.datetime.strptime(housework_datum["last_notification_date"], '%Y-%m-%d')
+        time = datetime.datetime.strptime(
+            housework["last_notification_date"], '%Y-%m-%d')
         time.isoformat()
         date = time.date()
         date.isoformat()
 
         # Calculate remaining days
-        if housework_datum["day_week_month"] == 'd':
+        if housework["day_week_month"] == 'd':
             # "day" is selected
-            housework_datum["remaining_days"] = (date - today).days + housework_datum["interval"]
-        if housework_datum["day_week_month"] == 'w':
+            housework["remaining_days"] = (
+                date - today).days + housework["interval"]
+        if housework["day_week_month"] == 'w':
             # "week" is selected
-            housework_datum["remaining_days"] = (date - today).days + housework_datum["interval"] * 7
-        if housework_datum["day_week_month"] == 'm':
+            housework["remaining_days"] = (
+                date - today).days + housework["interval"] * 7
+        if housework["day_week_month"] == 'm':
             # "month" is selected
-            housework_datum["remaining_days"] = (date - today).days + housework_datum["interval"] * 31
+            housework["remaining_days"] = (
+                date - today).days + housework["interval"] * 31
 
         # Update the last notification day if notification.py isn't running
-        if housework_datum["remaining_days"] < 0:
-            if housework_datum["day_week_month"] == 'd':
+        if housework["remaining_days"] < 0:
+            if housework["day_week_month"] == 'd':
                 # "day" is selected
-                housework_datum["remaining_days"] = housework_datum["interval"]
-            if housework_datum["day_week_month"] == 'w':
+                housework["remaining_days"] = housework["interval"]
+            if housework["day_week_month"] == 'w':
                 # "week" is selected
-                housework_datum["remaining_days"] = housework_datum["interval"] * 7
-            if housework_datum["day_week_month"] == 'm':
+                housework["remaining_days"] = housework["interval"] * 7
+            if housework["day_week_month"] == 'm':
                 # "month" is selected
-                housework_datum["remaining_days"] = housework_datum["interval"] * 31
+                housework["remaining_days"] = housework["interval"] * 31
 
-            db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?", today, housework_datum["id"])
+            db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?",
+                       today, housework["id"])
 
             flash("I'm sorry. I may not have been able to notify you last time.")
 
-
         # interval for display
-        if housework_datum["day_week_month"] == 'd':
+        if housework["day_week_month"] == 'd':
             # "day" is selected
-            housework_datum["interval_display"] = str(housework_datum["interval"]) + " day"
-        if housework_datum["day_week_month"] == 'w':
+            housework["interval_display"] = str(
+                housework["interval"]) + " day"
+        if housework["day_week_month"] == 'w':
             # "week" is selected
-            housework_datum["interval_display"] = str(housework_datum["interval"]) + " week"
-        if housework_datum["day_week_month"] == 'm':
+            housework["interval_display"] = str(
+                housework["interval"]) + " week"
+        if housework["day_week_month"] == 'm':
             # "month" is selected
-            housework_datum["interval_display"] = str(housework_datum["interval"]) + " month"
+            housework["interval_display"] = str(
+                housework["interval"]) + " month"
 
     # Sort housework data by remaining days
-    sorted_housework_data = sorted(housework_data, key=lambda x: x["remaining_days"])
+    sorted_housework = sorted(
+        housework, key=lambda x: x["remaining_days"])
 
-    return render_template("index.html", housework_data=sorted_housework_data, today=today)
+    return render_template("index.html", housework_data=sorted_housework, today=today)
 
 
 @app.route("/set", methods=["POST"])
 def set_token():
+    request_token = request.form.get("token")
+
     # Ensure LINE token was submitted
-    if not request.form.get("token"):
+    if not request_token:
         flash("Please input LINE token!")
         return redirect("/")
 
-    is_registerd = line("Your LINE token has been successfully registered.", request.form.get("token"))
+    is_registerd = line(
+        "Your LINE token has been successfully registered.", request_token)
 
     if is_registerd == 200:
         # Token is valid.
         token_data = db.execute("SELECT token FROM line_token")
         if len(token_data) == 0:
-            db.execute("INSERT INTO line_token (token) VALUES (?)", request.form.get("token"))
+            db.execute("INSERT INTO line_token (token) VALUES (?)",
+                       request_token)
         else:
-            db.execute("UPDATE line_token SET token = ?", request.form.get("token"))
+            db.execute("UPDATE line_token SET token = ?",
+                       request_token)
 
         flash("Your LINE token is registerd.")
 
@@ -143,9 +162,10 @@ def delete(delete_id):
     return redirect("/")
 
 
-def line(message, token):
+def line(message: str, token: str):
     # Send a message on LINE
     data = {'message': message}
     headers = {'Authorization': 'Bearer ' + token}
-    res = requests.post('https://notify-api.line.me/api/notify', data=data, headers=headers)
+    res = requests.post(
+        'https://notify-api.line.me/api/notify', data=data, headers=headers)
     return res.status_code
--- notification.py
+++ notification.py
@@ -7,11 +7,13 @@ import requests
 
 db = SQL("sqlite:///project.db")
 
-def line(message, token):
+
+def line(message: str, token: str):
     # Send a message on LINE
     data = {'message': message}
     headers = {'Authorization': 'Bearer ' + token}
-    res = requests.post('https://notify-api.line.me/api/notify', data=data, headers=headers)
+    res = requests.post(
+        'https://notify-api.line.me/api/notify', data=data, headers=headers)
 
 
 def regular_notifications():
@@ -23,18 +25,23 @@ def regular_notifications():
     # Notify me if it's the notification
     for housework_datum in housework_data:
         # Converts the last notification date to a date type
-        tdatetime = datetime.datetime.strptime(housework_datum["last_notification_date"], '%Y-%m-%d')
-        last_notification_date = datetime.date(tdatetime.year, tdatetime.month, tdatetime.day)
+        tdatetime = datetime.datetime.strptime(
+            housework_datum["last_notification_date"], '%Y-%m-%d')
+        last_notification_date = datetime.date(
+            tdatetime.year, tdatetime.month, tdatetime.day)
 
         # Set notification date
         if housework_datum["day_week_month"] == 'd':
-            reservation_date = last_notification_date + datetime.timedelta(days=housework_datum["interval"])
+            reservation_date = last_notification_date + \
+                datetime.timedelta(days=housework_datum["interval"])
 
         elif housework_datum["day_week_month"] == 'w':
-            reservation_date = last_notification_date + datetime.timedelta(days=housework_datum["interval"] * 7)
+            reservation_date = last_notification_date + \
+                datetime.timedelta(days=housework_datum["interval"] * 7)
 
         elif housework_datum["day_week_month"] == 'm':
-            reservation_date = housework_datum["last_notification_date"] + datetime.timedelta(days=housework_datum["interval"] * 31)
+            reservation_date = housework_datum["last_notification_date"] + \
+                datetime.timedelta(days=housework_datum["interval"] * 31)
 
         token_data = db.execute("SELECT token FROM line_token")
 
@@ -44,21 +51,26 @@ def regular_notifications():
             if today == reservation_date - datetime.timedelta(days=1):
                 # Notify when "week" or "month" are registerd.
                 if housework_datum["day_week_month"] == 'w' or housework_datum["day_week_month"] == 'm':
-                    line('Please do {} tomorrow!'.format(housework_datum["housework_name"]), token_data[0]["token"])
+                    line('Please do {} tomorrow!'.format(
+                        housework_datum["housework_name"]), token_data[0]["token"])
 
             # Notify if today is the reservation day.
             elif today == reservation_date:
-                line('Please do {} !'.format(housework_datum["housework_name"]), token_data[0]["token"])
+                line('Please do {} !'.format(
+                    housework_datum["housework_name"]), token_data[0]["token"])
                 # Update last notification date if "day" is registerd
                 if housework_datum["day_week_month"] == 'd':
-                    db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?;", reservation_date, housework_datum["id"])
+                    db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?;",
+                               reservation_date, housework_datum["id"])
 
             # Notify if today is the day after the reservation day.
             elif today == reservation_date + datetime.timedelta(days=1):
                 if housework_datum["day_week_month"] == 'w' or housework_datum["day_week_month"] == 'm':
-                    line('Did you do {} the day before? If you have not done that, you should do right now!'.format(housework_datum["housework_name"]), token_data[0]["token"])
+                    line('Did you do {} the day before? If you have not done that, you should do right now!'.format(
+                        housework_datum["housework_name"]), token_data[0]["token"])
                     # Update last notification date when "week" or "month" are registerd.
-                    db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?;", reservation_date + datetime.timedelta(days=1), housework_datum["id"])
+                    db.execute("UPDATE housework SET last_notification_date = ? WHERE id = ?;",
+                               reservation_date + datetime.timedelta(days=1), housework_datum["id"])
 
 
 # Scheduling to run at 10am
